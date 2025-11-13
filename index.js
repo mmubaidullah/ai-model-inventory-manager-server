03dfabfbb402d9f7980e1e50026262c634de6850
mmubaidullah <ubaid111999@gmail.com>
2025-11-13 23:33:56 +0600

    jachai commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const admin = require("firebase-admin");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 // ---middleware---
 
