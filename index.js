3d9dccabb4678641472bf70ab230084d99335317
mmubaidullah <ubaid111999@gmail.com>
2025-11-24 00:05:10 +0600

    Remove exposed MongoDB credentials

--- index.js
+++ index.js
@@ -42,8 +42,7 @@ const verifyFBToken = async (req, res, next) => {
 };
 
 // --------------------
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n1ywsso.mongodb.net/?appName=Cluster0";
-// const uri = "mongodb+srv://model-db:jwXIBxDFCphqfoRo@cluster0.n1ywsso.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n1ywsso.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
